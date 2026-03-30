b3a68c0ebaf5339e64c303a77335cb07ba065bab
nilmar pesaras <nilmar.pesarast@gmail.com>
2026-03-30 18:55:55 +0800

    REMOVE: old decision.tsx

--- src/routes/panelist/decisions.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/panelist/decisions')({
-  beforeLoad: () => {
-    throw redirect({
-      to: '/panelist',
-    })
-  },
-})

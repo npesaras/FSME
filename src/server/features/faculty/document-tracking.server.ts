51b878e561dda6e7ec586c046b523652607197b8
nilmar pesaras <nilmar.pesarast@gmail.com>
2026-03-30 18:56:08 +0800

    REMOVE: old server document functions

--- src/server/features/faculty/document-tracking.server.ts
+++
@@ -1,6 +0,0 @@
-import type { DocumentTrackingRecord } from '#/features/faculty/document-tracking'
-import { listDocumentTrackingRecordsForAccount } from '../shared/document-tracking.server'
-
-export async function listDocumentTrackingRecordsForFaculty(accountId: string) {
-  return listDocumentTrackingRecordsForAccount(accountId) as Promise<DocumentTrackingRecord[]>
-}
